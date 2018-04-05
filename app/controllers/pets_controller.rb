5e166ba16319535046c2fc6ce8fe618e030f69bc
victoria-huang <vctrah@gmail.com>
2018-04-04 23:19:30 -0400

    edit pets and owner edit/new views; added get/post routes

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,25 +2,55 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
-  post '/pets' do 
+  post '/pets' do
+    @pet = Pet.new(name: params[:pet][:name])
+
+    if !params[:owner][:name].empty?
+      owner = Owner.create(name: params[:owner][:name])
+      @pet.owner = owner
+    else
+      owner_id = params[:pet][:owner_id][0]
+      @pet.owner_id = owner_id
+    end
+
+    @pet.save
 
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  post '/pets/:id' do 
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    @owners = Owner.all
+    erb :"pets/edit"
+  end
 
+  post '/pets/:id' do
+    @pet = Pet.find(params[:id])
+
+    @pet.name = params[:pet][:name]
+
+    if !params[:owner][:name].empty?
+      owner = Owner.create(name: params[:owner][:name])
+      @pet.owner = owner
+    else
+      owner_id = params[:pet][:owner_id][0]
+      @pet.owner_id = owner_id
+    end
+
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 end
